db7e41fd2baaf1e000b537627e8d371e03e1475b
ylc <756885686@qq.com>
2024-06-04 23:44:17 +0800

    引入store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from "./router";
+import { Provider } from 'react-redux'
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}></RouterProvider>,
+  <Provider store={store}>
+    <RouterProvider router={router}></RouterProvider>,
+  </Provider>
 )
 
 // Use contextBridge
