e5ab52d8d7e4d0da963a2461f114bd6f10919195
袁隆成 <756885686@qq.com>
2024-06-07 16:13:07 +0800

    调整模板

--- src/pages/Resume/ResumeToolbar/index.tsx
+++ src/pages/Resume/ResumeToolbar/index.tsx
@@ -1,5 +1,25 @@
+import ScrollBox from '@/components/ScrollBox'
+import RESUME_TOOLBAR_LIST from '@/constants/resume.ts';
 import './index.scss'
 const ResumeToolbar = () => {
-    return <div>我是简历工作条操作区</div>;
+    const height = document.body.clientHeight;
+    return (
+        <div className="slider">
+            <ScrollBox maxHeight={height - 180}>
+                <div className="module">
+                    全部模块
+                    <div className="content">
+                        {RESUME_TOOLBAR_LIST.map((toolbar: TSResume.SliderItem) => {
+                            return (
+                                <div className="box" key={toolbar.key}>
+                                    // ...
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </ScrollBox>
+        </div>
+    );
 }
 export default ResumeToolbar;
