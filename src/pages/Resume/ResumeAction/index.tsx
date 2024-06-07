f1650b46db5725f613c865449f1168d455d4518f
袁隆成 <756885686@qq.com>
2024-06-07 11:25:15 +0800

    简历页面头部

--- src/pages/Resume/ResumeAction/index.tsx
+++ src/pages/Resume/ResumeAction/index.tsx
@@ -1,5 +1,17 @@
 import './index.scss'
+import { useNavigate } from 'react-router'
+import MyButton from '@/components/Button'
 const ResumeAction = () => {
-    return <div>我是简历头部操作区</div>;
+    const navigate = useNavigate();
+    // 导出PDF
+    const onExport = () => {
+        console.log('导出pdf');
+    };
+    return (
+        <div className={'actions'}>
+            <div className={'back'} onClick={() => navigate('/')}>返回</div>
+            <MyButton size="middle" className="export-btn" onClick={onExport}>导出PDF</MyButton>
+        </div>
+    )
 }
 export default ResumeAction;
