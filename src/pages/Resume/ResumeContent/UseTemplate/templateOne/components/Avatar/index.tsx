051bc1ae2d2961c8b8b6cad402aa0f3b47dcc181
ylc <756885686@qq.com>
2024-06-19 00:12:38 +0800

    头像上传

--- src/pages/Resume/ResumeContent/UseTemplate/templateOne/components/Avatar/index.tsx
+++ src/pages/Resume/ResumeContent/UseTemplate/templateOne/components/Avatar/index.tsx
@@ -2,15 +2,47 @@
  * @desc 头像
  * @author pengdaokuan
  */
-import './index.scss';
+import style from './index.module.scss';
 import AvatarImage from '@/assets/react.svg';
+import { useSelector } from 'react-redux';
+import uploadIcon from '@/assets/icon/upload.png';
+import Button from '@/components/Button';
+import ImageUpload from '@/components/Upload/ImageUpload';
+import useUpdateResume from '@/hooks/useUpdateResume';
+import {State} from "@/type/storeState.ts";
 
-function Avatar() {
+const Avatar = () => {
+
+  const updateResume = useUpdateResume();
+  const base: TSResume.Base = useSelector((state: State) => state.resumeModel.base);
+
+  // 更新用户的简历头像
+  const onUpdateUserAvatar = (avatarUrl: string) => {
+    updateResume<string>('base/avatar', avatarUrl);
+  };
+
   return (
-    <div className="box">
-      <div className="avatar">
-        <img src={AvatarImage}  alt={''}/>
-      </div>
+    <div className={style.box}>
+      {!base?.avatar && (
+        <ImageUpload
+          icon={uploadIcon}
+          accept="image/*"
+          multiple={false}
+          onAfterChange={(files: TSUpload.File[]) => {
+            onUpdateUserAvatar(files[0]?.base64URL);
+          }}
+        />
+      )}
+      {base?.avatar && (
+        <div className={style.avatar}>
+          <img src={base?.avatar || AvatarImage} alt={''}/>
+          <div className={style.mask}>
+            <Button size="small" className={style.btnChange} onClick={() => onUpdateUserAvatar('')}>
+              更换
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
