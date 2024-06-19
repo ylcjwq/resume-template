4ddd536bf36d638b23cb335ce664052f4951de0d
ylc <756885686@qq.com>
2024-06-19 22:46:01 +0800

    导出pdf简历

--- src/pages/Resume/ResumeAction/index.tsx
+++ src/pages/Resume/ResumeAction/index.tsx
@@ -1,17 +1,25 @@
 import './index.scss'
 import { useNavigate } from 'react-router'
-import MyButton from '@/components/Button'
+import Button from '@/components/Button'
+import {useSelector} from "react-redux";
+import {State} from "@/type/storeState.ts";
+import toPrintPdf from "@/utils/htmlToPDF.ts";
 const ResumeAction = () => {
-    const navigate = useNavigate();
-    // 导出PDF
-    const onExport = () => {
-        console.log('导出pdf');
-    };
-    return (
-        <div className={'actions'}>
-            <div className={'back'} onClick={() => navigate('/')}>返回</div>
-            <MyButton size="middle" className="export-btn" onClick={onExport}>导出PDF</MyButton>
-        </div>
-    )
+
+  const navigate = useNavigate();
+  const base: TSResume.Base = useSelector((state: State) => state.resumeModel.base);
+
+  // 导出PDF
+  const onExport = () => {
+    console.log('导出pdf');
+    toPrintPdf(`${base?.username}简历`);
+  };
+
+  return (
+    <div className={'actions'}>
+      <div className={'back'} onClick={() => navigate('/')}>返回</div>
+      <Button size="middle" className="export-btn" onClick={onExport}>导出PDF</Button>
+    </div>
+  )
 }
 export default ResumeAction;
