6f2368083bd75fd7384fa8faebdfefd944c1d599
ylc <756885686@qq.com>
2024-07-18 13:03:41 +0800

    简历模板列表样式响应式

--- src/pages/TemplateList/Navigation/index.tsx
+++ src/pages/TemplateList/Navigation/index.tsx
@@ -4,9 +4,21 @@ import TemplateCoverOne from '@/assets/template/template1.png';
 import TemplateCoverTwo from '@/assets/template/template2.png';
 import ScrollBox from '@/components/ScrollBox';
 import Button from '@/components/Button';
+import {useEffect, useState} from "react";
 
 const Navigation = () => {
-  const height = document.body.clientHeight;
+  const [height, setHeight] = useState(window.innerHeight);
+  // 监听窗口大小变化
+  const handleResize = () => {
+    setHeight(window.innerHeight); // 更新height状态
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  },[])
 
   return (
     <div className={style.navigation}>
--- src/pages/TemplateList/StaticResume/index.tsx
+++ src/pages/TemplateList/StaticResume/index.tsx
@@ -2,10 +2,22 @@ import style from './index.module.scss'
 import * as TemplateList from '@/pages/templates';
 import Footer from '../Footer';
 import ScrollBox from '@/components/ScrollBox';
+import {useEffect, useState} from "react";
 
 const StaticResume = () => {
   const HEADER_HEIGHT = 76; // 距离头部距离
-  const height = document.body.clientHeight;
+  const [height, setHeight] = useState(window.innerHeight);
+  // 监听窗口大小变化
+  const handleResize = () => {
+    setHeight(window.innerHeight); // 更新height状态
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  },[])
 
   return (
     <div className={style.container}>
