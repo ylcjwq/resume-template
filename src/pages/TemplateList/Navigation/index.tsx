b29bc09dc8acc28c15ea8f66b5dce962d6a13e19
ylc <756885686@qq.com>
2024-07-19 15:22:08 +0800

    优化模板列表

--- src/pages/TemplateList/Navigation/index.tsx
+++ src/pages/TemplateList/Navigation/index.tsx
@@ -1,16 +1,26 @@
 import style from './index.module.scss'
 import UseIcon from '@/assets/icon/use.png';
-import TemplateCoverOne from '@/assets/template/template1.png';
-import TemplateCoverTwo from '@/assets/template/template2.png';
 import ScrollBox from '@/components/ScrollBox';
 import Button from '@/components/Button';
 import {useEffect, useState} from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import {TemplateState} from "@/type/storeState.ts";
+import {setSelectTemplate} from "@/store/modules/templateModel.ts";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+  const HEADER_HEIGHT = 92;
   const [height, setHeight] = useState(window.innerHeight);
+  const templateList: TSTemplate.Item[] = useSelector((state: TemplateState) => state.templateModel.templateList);
+  const selectTemplate: TSTemplate.Item = useSelector((state: TemplateState) => state.templateModel.selectTemplate);
+
   // 监听窗口大小变化
   const handleResize = () => {
     setHeight(window.innerHeight); // 更新height状态
+  };
+
+  const onChangeTemplate = (template: TSTemplate.Item) => {
+    dispatch(setSelectTemplate(template));
   };
 
   useEffect(() => {
@@ -22,23 +32,26 @@ const Navigation = () => {
 
   return (
     <div className={style.navigation}>
-      <ScrollBox maxHeight={height - 60 - 32}>
-        {/* 悬浮效果一：属于当前模版 */}
-        <div className={style.template}>
-          <img className={style.cover} src={TemplateCoverOne} alt="cover" />
-          <div className={style.mask}>
-            <img className={style.use} src={UseIcon} alt="cover"/>
-          </div>
-        </div>
-        {/* 悬浮效果二：可选择预览模版 */}
-        <div className={style.template}>
-          <img className={style.cover} src={TemplateCoverTwo} alt="cover"/>
-          <div className={style.mask}>
-            <Button size="middle" className={style.viewBtn} onClick={() => {}}>
-              预览模版
-            </Button>
-          </div>
-        </div>
+      <ScrollBox maxHeight={height - HEADER_HEIGHT}>
+        {templateList &&
+          templateList.length > 0 &&
+          templateList.map((template: TSTemplate.Item) => {
+            return (
+              <div className={style.template} key={template?.templateId}>
+                <img className={style.cover} src={template?.templateCover} alt={''}/>
+                <div className={style.mask}>
+                  {selectTemplate?.templateId === template?.templateId && (
+                    <img className={style.use} src={UseIcon} alt={''}/>
+                  )}
+                  {selectTemplate?.templateId !== template?.templateId && (
+                    <Button size="middle" className={style.viewBtn} onClick={() => { onChangeTemplate(template) }}>
+                      预览模版
+                    </Button>
+                  )}
+                </div>
+              </div>
+            );
+          })}
       </ScrollBox>
     </div>
   );
