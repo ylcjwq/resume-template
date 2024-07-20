9871e8fcda0195e49876bd9623a0c5427fb70855
ylc <756885686@qq.com>
2024-07-20 17:40:31 +0800

    主题色仓库

--- src/type/storeState.ts
+++ src/type/storeState.ts
@@ -1,9 +1,13 @@
 import resumeReducer from "@/store/modules/resumeModel.ts";
 import templateReducer from "@/store/modules/templateModel.ts";
+import ThemeReducer from "@/store/modules/themeModel.ts";
 
 export interface State {
   resumeModel: ReturnType<typeof resumeReducer>
 }
 export interface TemplateState {
   templateModel: ReturnType<typeof templateReducer>
+}
+export interface ThemeState {
+  themeModel: ReturnType<typeof ThemeReducer>
 }
