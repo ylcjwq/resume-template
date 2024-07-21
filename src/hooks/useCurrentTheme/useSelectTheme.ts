afcf8fb4a4effd8771e6a97f539ae5c6e1324c14
ylc <756885686@qq.com>
2024-07-21 12:16:27 +0800

    封装主题色对比hook

---
+++ src/hooks/useCurrentTheme/useSelectTheme.ts
@@ -0,0 +1,26 @@
+import {useDispatch} from "react-redux";
+import _ from "lodash";
+import {setCurrentTheme, setThemeList} from "@/store/modules/themeModel.ts";
+
+/**
+ * @description 对比上一轮的选中的主题皮肤
+ */
+function useSelectTheme() {
+  const dispatch = useDispatch();
+  return (themeConfigValues: any) => {
+    const prevTheme: string = themeConfigValues?.currentTheme || '';
+    const initTheme = { id: 'dark', fontColor: '#ffffff', backgroundColor: '#27292c' };
+
+    let nextTheme: TSTheme.Item;
+    if (themeConfigValues?.themeList.length > 0) {
+      if (prevTheme) nextTheme = _.find(themeConfigValues?.themeList, { id: prevTheme }) || initTheme;
+      else nextTheme = themeConfigValues?.themeList[0];
+    } else {
+      nextTheme = initTheme;
+    }
+    dispatch(setThemeList(themeConfigValues?.themeList));
+    dispatch(setCurrentTheme(nextTheme));
+  };
+}
+
+export default useSelectTheme
