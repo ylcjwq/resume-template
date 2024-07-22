346e09759ec973ed2e87ae4190a940ac330d71dd
ylc <756885686@qq.com>
2024-07-23 00:29:24 +0800

    修改路径窗口

--- src/windowPages/setting/App.tsx
+++ src/windowPages/setting/App.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+import Button from '@/components/Button';
+import style from './index.module.scss'
 
-function Setting() {
-  return <div>应用设置-新窗口</div>;
+const Setting = () => {
+  const [resumeSavePath, setResumeSavePath] = useState('');
+
+  const onSave = () => {};
+  return (
+    <div className={style.container}>
+      <p className={style.label}>修改简历数据储存路径</p>
+      <div className={style.input}>
+        <div className={style.value}>{resumeSavePath || '请选择存储路'}</div>
+        <div className={style.updateBtn}>更改路径</div>
+      </div>
+    </div>
+  );
 }
 
 export default Setting;
