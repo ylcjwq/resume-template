70c7a5b25a9a9375a5f63bcbfa06b08e6777a251
ylc <756885686@qq.com>
2024-07-22 22:49:58 +0800

    引入多窗口

--- electron/main.ts
+++ electron/main.ts
@@ -16,6 +16,7 @@ export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
 
 let win: BrowserWindow | null
+let setting: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
@@ -25,16 +26,31 @@ function createWindow() {
     },
   })
 
+  setting = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    width: 720,
+    height: 240,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.mjs'),
+    },
+  })
+  // 隐藏顶部菜单栏
+  setting.setMenu(null);
+
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())
   })
 
   if (VITE_DEV_SERVER_URL) {
+    console.log(VITE_DEV_SERVER_URL)
     win.loadURL(VITE_DEV_SERVER_URL)
+    setting.loadURL(path.join(VITE_DEV_SERVER_URL,'/src/windowPages/setting/index.html'))
   } else {
     // win.loadFile('dist/index.html')
     win.loadFile(path.join(RENDERER_DIST, 'index.html'))
+    win.loadFile(path.join(RENDERER_DIST, '/src/windowPages/setting/index.html'))
   }
 }
 
