6c34fee09e64430cc9e4d4c90a96fa084f757f8d
ylc <756885686@qq.com>
2024-07-24 00:05:21 +0800

    配置窗口初始化路径

--- src/windowPages/setting/App.tsx
+++ src/windowPages/setting/App.tsx
@@ -1,14 +1,34 @@
 import { useState } from 'react';
 import style from './index.module.scss'
+import {useReadGlobalConfigFile, useUpdateGlobalConfigFile} from "@/hooks/useGlobalConfigActionHooks.ts";
+import useMount from "@/hooks/useMount.ts";
+import getAppPath from "@/utils/appPath.ts";
 
 const Setting = () => {
   const [resumeSavePath, setResumeSavePath] = useState('');
+  const readAppConfigThemeFile = useReadGlobalConfigFile();
+  const updateGlobalConfigFile = useUpdateGlobalConfigFile();
+
+  useMount(() => {
+    readAppConfigThemeFile().then((value: { [key: string]: string }) => {
+      if (value?.resumeSavePath) {
+        setResumeSavePath(value?.resumeSavePath);
+      } else {
+        // 不存在默认路径，则设置默认路径并更新文件内容
+        getAppPath().then((appPath: string) => {
+          setResumeSavePath(`${appPath}\\resumeCache`);
+          updateGlobalConfigFile('resumeSavePath', `${appPath}\\resumeCache`);
+        });
+      }
+    });
+  });
 
   const onChangePath = () => {
     window.electron.ipcRenderer.send('open-save-resume-path', '');
     window.electron.ipcRenderer.on('save-resume-path-reply', (_, arg: string) => {
       if(arg) {
         setResumeSavePath(arg);
+        updateGlobalConfigFile('resumeSavePath', arg);
       } else {
         new Error('自定义存储路径失败！')
       }
