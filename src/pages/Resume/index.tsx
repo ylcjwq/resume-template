7afec2bf97371db41b2caf554ba67b2194646df8
ylc <756885686@qq.com>
2024-07-26 19:29:22 +0800

    删除多余代码

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,16 +1,9 @@
-import getAppPath from "@/utils/appPath.ts";
 import ResumeAction from './ResumeAction';
 import ResumeContent from './ResumeContent';
 import ResumeToolbar from './ResumeToolbar';
 import './index.scss'
 
 const Resume = () => {
-    getAppPath().then(appPath => {
-      window.electron.ipcRenderer.send('fileRead', `${appPath}/src/App.tsx`);
-    })
-    window.electron.ipcRenderer.on('fileReadReply', (_, data) => {
-        data
-    })
     return (
         <div className={'containerBox'}>
             <div className={'header'}>
